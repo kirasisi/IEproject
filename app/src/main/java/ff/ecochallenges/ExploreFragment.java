71944d1f75533850a3577dea02e021e396e16ac0
kirasisi <wenfei.jiang@yahoo.com>
2018-09-08 18:58:36 +1000

    update: added perCapita to explore screen,made simultaneous changing between year spinner and checkbox

--- app/src/main/java/ff/ecochallenges/ExploreFragment.java
+++ app/src/main/java/ff/ecochallenges/ExploreFragment.java
@@ -15,8 +15,11 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -37,6 +40,10 @@ public class ExploreFragment extends Fragment {
     private ImageView closeBtn;
     PieChart piechart;
     DatabaseReference db;
+    CheckBox cb;
+    Spinner yearSelect;
+    String year=null;
+    TextView title;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,61 +55,61 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View vExplore = inflater.inflate(R.layout.fragment_explore, container, false);
-        final Spinner yearSelect = (Spinner)vExplore.findViewById(R.id.selectYear);
+        yearSelect = (Spinner)vExplore.findViewById(R.id.selectYear);
         piechart = vExplore.findViewById(R.id.pieC);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.yearOfChart,android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         yearSelect.setAdapter(adapter);
+        cb = vExplore.findViewById(R.id.checkBox);
+        title = vExplore.findViewById(R.id.titleForTotalChart);
+        year = yearSelect.getSelectedItem().toString();
+        getData(year);
 
 
 
-        yearSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String year = yearSelect.getSelectedItem().toString();
-                db = FirebaseDatabase.getInstance().getReference().child("ODTotalAnnualWaste");
-                db.orderByKey().equalTo(year).addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                        Double glass = dataSnapshot.child("Glass").child("totalGenerated").getValue(Double.class);
-                        Double metal = dataSnapshot.child("Metal").child("totalGenerated").getValue(Double.class);
-                        Double organic = dataSnapshot.child("Organic").child("totalGenerated").getValue(Double.class);
-                        Double paper = dataSnapshot.child("Paper").child("totalGenerated").getValue(Double.class);
-                        Double plastic = dataSnapshot.child("Plastic").child("totalGenerated").getValue(Double.class);
-                        Double rubber = dataSnapshot.child("Rubber").child("totalGenerated").getValue(Double.class);
-                        Double textile = dataSnapshot.child("Textile").child("totalGenerated").getValue(Double.class);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(cb.isChecked()){
+                    yearSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                             @Override
+                                                             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                                 year = yearSelect.getSelectedItem().toString();
+
+                                                             }
+
+                                                             @Override
+                                                             public void onNothingSelected(AdapterView<?> parent) {
+
+                                                             }
+                                                         });
+                    title.setText("Annually Generated Waste Per Capita (KG), Victoria");
+
+                    getPerCap(year);
 
 
-                        setPie(glass,metal,organic,paper,plastic,rubber,textile);
-                    }
+                }
+                else{
+                    yearSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            year = yearSelect.getSelectedItem().toString();
 
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        }
 
-                    }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
 
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
+                        }
+                    });
+                    title.setText("Annually Generated Total Waste (Tonnes), Victoria");
+                    getData(year);
 
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+                }
             }
         });
 
@@ -110,8 +117,126 @@ public class ExploreFragment extends Fragment {
         return vExplore;
     }
 
+    public void getPerCap(String year){
 
-    public void setPie(double glassTotal,double metalTotal,double organicToal,double paperTotal,double plasticTotal, double rubberTotal, double textTotal) {
+            setPerCap(year);
+
+        yearSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String year = yearSelect.getSelectedItem().toString();
+                setPerCap(year);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });}
+
+
+    public void getData(String year){
+           setData(year);
+
+            yearSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String year = yearSelect.getSelectedItem().toString();
+                    setData(year);
+
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+
+    public void setPerCap(String year){
+        db = FirebaseDatabase.getInstance().getReference().child("ODTotalAnnualWaste");
+        db.orderByKey().equalTo(year).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Double glass = dataSnapshot.child("Glass").child("generatedPerCapitaKg").getValue(Double.class);
+                Double metal = dataSnapshot.child("Metal").child("generatedPerCapitaKg").getValue(Double.class);
+                Double organic = dataSnapshot.child("Organic").child("generatedPerCapitaKg").getValue(Double.class);
+                Double paper = dataSnapshot.child("Paper").child("generatedPerCapitaKg").getValue(Double.class);
+                Double plastic = dataSnapshot.child("Plastic").child("generatedPerCapitaKg").getValue(Double.class);
+                Double rubber = dataSnapshot.child("Rubber").child("generatedPerCapitaKg").getValue(Double.class);
+
+
+
+                setPie(glass,metal,organic,paper,plastic,rubber);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void setData(String year){
+        db = FirebaseDatabase.getInstance().getReference().child("ODTotalAnnualWaste");
+        db.orderByKey().equalTo(year).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Double glass = dataSnapshot.child("Glass").child("totalGenerated").getValue(Double.class);
+                Double metal = dataSnapshot.child("Metal").child("totalGenerated").getValue(Double.class);
+                Double organic = dataSnapshot.child("Organic").child("totalGenerated").getValue(Double.class);
+                Double paper = dataSnapshot.child("Paper").child("totalGenerated").getValue(Double.class);
+                Double plastic = dataSnapshot.child("Plastic").child("totalGenerated").getValue(Double.class);
+                Double rubber = dataSnapshot.child("Rubber").child("totalGenerated").getValue(Double.class);
+
+
+
+                setPie(glass,metal,organic,paper,plastic,rubber);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    public void setPie(double glassTotal,double metalTotal,double organicToal,double paperTotal,double plasticTotal, double rubberTotal) {
         piechart.setHoleColor(Color.WHITE);
         ArrayList<PieEntry> entries = new ArrayList<>();
         entries.add(new PieEntry((float) metalTotal, "Metal"));
@@ -132,14 +257,16 @@ public class ExploreFragment extends Fragment {
         piechart.setData(pieData);
         piechart.invalidate();
         //piechart.highlightValue(type, 0, false);
-        dataSet.setColors(new int[]{Color.parseColor("#c61939"),
-                Color.parseColor("#af4623"),
-                Color.parseColor("#6a753b"),
-                Color.parseColor("#FFF2ED53"),
-                Color.parseColor("#3c6bb7"),
-                Color.parseColor("#a83ca2"),
+        dataSet.setColors(new int[]{Color.parseColor("#b79a96"),
+                Color.parseColor("#73d0f4"),
+                Color.parseColor("#81db6d"),
+                Color.parseColor("#ed8a12"),
+                Color.parseColor("#ffc1c3"),
+                Color.parseColor("#d4a2f2"),
         });
     }
+
+
 
 
 
---
+++ app/src/main/java/ff/ecochallenges/drillDown.java
@@ -0,0 +1,32 @@
+package ff.ecochallenges;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class drillDown extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vDrill = inflater.inflate(R.layout.fragment_drill_down, container, false);
+
+        return vDrill;
+    }
+
+
+
+}
