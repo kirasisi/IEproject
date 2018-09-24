cb93b42ae87226d54603bf45da19f087a160ad69
kirasisi <wenfei.jiang@yahoo.com>
2018-09-24 14:06:38 +1000

    updated: pass user point to game and in game shop

--- game/src/main/java/ff/ecochallenges/game/pointTransfer.java
+++ game/src/main/java/ff/ecochallenges/game/pointTransfer.java
@@ -3,26 +3,21 @@ package ff.ecochallenges.game;
 import android.content.SharedPreferences;
 import android.util.Log;
 
-import com.google.firebase.database.DatabaseReference;
-
 public class pointTransfer {
-    DatabaseReference db;
 
     private SharedPreferences myPreferences;
     private int point=0;
-    public void setPoint(int p){
-        point = p;
-        Log.d("Unity",String.valueOf(point));
-    }
+
     public pointTransfer(){
 
 
     }
-    public void pointPass(){
-
-         Global g = Global.getInstance();
+    public int pointPass(){
+        Global g = Global.getInstance();
         int data=g.getData();
         Log.d("Unity",String.valueOf(data));
+        return data;
 
     }
+
 }
